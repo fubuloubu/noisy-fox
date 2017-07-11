1d4f9bd6b02f42229cba2944bf55746668916b00
Bryant <fubuloubu@gmail.com>
2017-07-11 14:44:50 -0700

    Added full test case suite and fixed some issues with functions

--- testcase.py
+++ testcase.py
@@ -11,13 +11,11 @@ class TestCaseContentManager(XMLContentManager):
         test_template = {
             'root' : {
                 'testcase' : OrderedDict([
-                    ('@uid', random_hash()),
+                    # So we have a trackable UID that never updates
+                    # in case there is every confusion based on named changes, etc.
+                    ('@uid', random_hash()), 
                     ('title', 'Enter a Title'),
-                    ('description', 'Enter a Description'),
-                    ('setup', {}),
-                    ('action', {}),
-                    ('result', {}),
-                    ('verify', {})
+                    ('description', 'Enter a Description')
                 ])
             }
         }
@@ -25,7 +23,7 @@ class TestCaseContentManager(XMLContentManager):
         super().__init__(filename, new_contents=new_contents)
 
     # Default Testcase Data GET/SET
-    def get_uid(self):
+    def get_uid(self): # Note: no setter, should not be updated!
         return self.get('root/testcase/@uid')
 
     def get_title(self):
@@ -39,49 +37,125 @@ class TestCaseContentManager(XMLContentManager):
         self.set('root/testcase/description', new_description)
 
     # Testcase sectional data GET/SET
-    def create_signal_obj(self, section, name, datapoints, data_fmt='.03f'):
+    def create_signal_obj(self, section, name, \
+            value=0.00000, timeseries=None, \
+            latency=0.00000, sample_time=0.00100, \
+            lsb=1.000, variance=0.000, \
+            time_fmt = '.05f', data_fmt='.03f'):
+        
+        # Name is required, no matter what
+        sig = {}
         sig['@name'] = name
         
-        # TODO: Identify additional attributes here
-        # Might be per-customer
-        time_fmt = '.03f'
-        if section in ['']:
-            sig['@latency'] = format(0, time_fmt)
-            sig['@update-rate'] = format(0, time_fmt)
-            sig['@lsb'] = format(0, data_fmt)
-            sig['@precision'] = format(0, data_fmt)
+        # Depending on the section we're talking about,
+        # different attributes apply
+        # Note: Attributes might be per-customer
+        if section in ['action', 'result', 'verify']:
+            sig['@sample_time'] = format(sample_time, time_fmt)
+            sig['@lsb'] = format(lsb, data_fmt)
+        if section in ['verify']:
+            sig['@latency'] = format(latency, time_fmt)
+            sig['@variance'] = format(variance, data_fmt)
         
-        fmt = lambda time, data: {'@time' : format(time, time_fmt), '@data' : format(data, data_fmt)}
-        # Note: Timeseries object is default object. Perhaps make this extensible for other types?
-        # e.g. per-customer
-        sig['timeseries'] = [fmt(pt) for pt in datapoints]
+        # The value of the signal is either a timeseries
+        if section in ['action', 'result', 'verify']:
+            # Note: Timeseries object is default object. 
+            # Perhaps make this extensible for other types? e.g. per-customer
+            fmt = lambda time, data: { 
+                    '@time' : format(time, time_fmt), 
+                    '@data' : format(data, data_fmt)
+            }
+            sig['timeseries'] = [fmt(t,d) for t,d in timeseries]
+        # or a static value
+        elif section in ['setup']:
+            sig['@value'] = format(value, data_fmt)
         
+        #else: This should be an appliation error
+
         return sig
 
     def get_section_objs(self, section):
         return self.get('root/testcase/' + section)
-        return sig_template
 
     def find_section_obj_idx(self, section, name):
-        for i, obj in iter(self.get_section_objs(section)):
+        for i, obj in enumerate(self.get_section_objs(section)):
             if obj['@name'] == name:
                 return i
 
-    def add_section_obj(self, section, name):
-        sig = self.create_signal_obj(section, name, datapoints)
+    def add_section_obj(self, section, name, data):
+        sig = self.create_signal_obj(section, name, **data)
         self.set('root/testcase/' + section + '/-1', sig)
 
     def del_section_obj(self, section, name):
-        idx = find_section_obj_idx(section, name)
-        self.set('root/testcase/' + section + '/' + idx, 'pop')
+        idx = str(self.find_section_obj_idx(section, name))
+        self.get('root/testcase/' + section + '/' + idx + '/#pop')
 
-    def update_section_obj(self, section, name, datapoints):
-        sig = self.create_signal_obj(section, name, datapoints)
-        idx = find_section_obj_idx(section, name)
+    def update_section_obj(self, section, name, data):
+        sig = self.create_signal_obj(section, name, **data)
+        idx = str(self.find_section_obj_idx(section, name))
         self.set('root/testcase/' + section + '/' + idx, sig)
 
 if __name__ == '__main__':
+    # Test this class
     test = TestCaseContentManager('test.xml')
-    test._write()
+    uid = str(test.get_uid())
+    assert(len(uid) != 0)
+    new_title = test.get_title() + uid
+    test.set_title(new_title)
+    assert(test.get_title() == new_title)
+    new_description = test.get_description() + uid
+    test.set_description(new_description)
+    assert(test.get_description() == new_description)
+    
+    signame1 = 'sig-name-1'
+    signame2 = 'sig-name-2'
+    data1 = {'value' : 1.000, 'sample_time' : 0.100, 'lsb' : 0.500, 
+            'latency' : 0.300, 'variance' : 0.250,
+            'timeseries' : [(0, 0), (1, 1), (2, 2), (3, 3)]
+    }
+    data2 = {'value' : 2.000, 'sample_time' : 0.200, 'lsb' : 1.000, 
+            'latency' : 0.400, 'variance' : 0.500,
+            'timeseries' : [(0, 1), (1, 2), (2, 3), (3, 4)]
+    }
+    
+    for section in ['setup', 'action', 'result', 'verify']:
+        test.add_section_obj(section, signame1, data1)
+        idx = test.find_section_obj_idx(section, signame1)
+        assert(idx == 0)
+        assert(test.get_section_objs(section)[idx]['@name'] == signame1)
+        obj = test.get_section_objs(section)[idx]
+        for key in obj.keys():
+            if key[0] == '@' and key != '@name':
+                assert(float(obj[key]) == data1[key.strip('@')])
+            if key == 'timeseries':
+                for i, pt in enumerate(data1[key]):
+                    assert(float(obj[key][i]['@time']) == pt[0])
+                    assert(float(obj[key][i]['@data']) == pt[1])
+        test.update_section_obj(section, signame1, data2)
+        obj = test.get_section_objs(section)[idx]
+        for key in obj.keys():
+            if key[0] == '@' and key != '@name':
+                assert(float(obj[key]) == data2[key.strip('@')])
+            if key == 'timeseries':
+                for i, pt in enumerate(data2[key]):
+                    assert(float(obj[key][i]['@time']) == pt[0])
+                    assert(float(obj[key][i]['@data']) == pt[1])
+        test.add_section_obj(section, signame2, data1)
+        idx = test.find_section_obj_idx(section, signame2)
+        assert(idx == 1)
+        assert(test.get_section_objs(section)[idx]['@name'] == signame2)
+        obj = test.get_section_objs(section)[idx]
+        for key in obj.keys():
+            if key[0] == '@' and key != '@name':
+                assert(float(obj[key]) == data1[key.strip('@')])
+            if key == 'timeseries':
+                for i, pt in enumerate(data1[key]):
+                    assert(float(obj[key][i]['@time']) == pt[0])
+                    assert(float(obj[key][i]['@data']) == pt[1])
+        test.del_section_obj(section, signame2)
+        test.del_section_obj(section, signame1)
+        assert(len(test.get_section_objs(section)) == 0)
+    
+    # Completed testing
     test.delete()
     print("TestCaseContentManager() Testing Passed!")
