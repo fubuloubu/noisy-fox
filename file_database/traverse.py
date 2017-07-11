e674aeeffccfb5e4ffcd8dfac36d19f5742af6ee
Bryant <fubuloubu@gmail.com>
2017-07-11 14:45:38 -0700

    Needed a bit more logic to work, added test cases for new logic

--- file_database/traverse.py
+++ file_database/traverse.py
@@ -8,7 +8,16 @@ def traverse(tree, path, value=None):
     new_path = key_split[-1]
     # If our tree is a dict
     if isinstance(tree, dict):
-        # check if our split did not split further.
+        # first check the current key exists in tree
+        if key not in tree.keys():
+            # Check if the next_path would be a list or dict
+            if new_path[0].isalpha():
+                # If the first char is a letter, than key is for a dict
+                tree[key] = dict()
+            else:
+                # else it's for a list.
+                tree[key] = list()
+        # then check if our split did not split further.
         if new_path == path:
             # If yes and we are trying to set
             if value:
@@ -26,21 +35,27 @@ def traverse(tree, path, value=None):
             # If yes and we are trying to set
             if value:
                 # then set
-                if path == '-1':
+                if path == '-1' or int(path) == len(tree):
                     # '-1' marks extension
-                    tree.extend(value)
+                    if isinstance(value, list):
+                        tree.extend(value)
+                    else:
+                        tree.append(value)
                 else:
                     # otherwise just directly set
                     tree[int(path)] = value
                 return
             # else return value at that path
-            if path.strip('-').isdigit():
-                # process a digit normally
+            if path.strip('-').isdigit(): #pos/neg int
+                # if int, process a digit normally
                 return tree[int(path)]
             else:
                 # if non-numerical, attempt parsing as a selector
                 [start,end] = [int(s) for s in path.split(':')]
                 return tree[start:end]
+        # If we are directed to pop a key, do that
+        if new_path == '#pop':
+            return tree.pop(int(key))
         # If our split did work, continue traversal.
         return traverse(tree[int(key)], new_path, value=value)
     # If tree is neither dict nor list, 
@@ -50,7 +65,7 @@ def traverse(tree, path, value=None):
 if __name__ == '__main__':
     # run tests
     test_tree = {}
-    traverse(test_tree, 'root', value={'nested' : {}})
+    traverse(test_tree, 'root/nested')
     assert(False if traverse(test_tree, 'root/nested') else True)
     traverse(test_tree, 'root/nested/multiple', value='times')
     assert(traverse(test_tree, 'root/nested/multiple') == 'times')
@@ -58,7 +73,8 @@ if __name__ == '__main__':
     assert(len(traverse(test_tree, 'root/branch')) == 2)
     assert(traverse(test_tree, 'root/branch/0') == 'A')
     assert(traverse(test_tree, 'root/branch/1') == 'B')
-    traverse(test_tree, 'root/new', value={'branch': ['C', 'd'] })
+    traverse(test_tree, 'root/new/branch/-1', value='C') #appends value
+    traverse(test_tree, 'root/new/branch/1', value='d') #handles index out of range
     assert(traverse(test_tree, 'root/new/branch/0') == 'C')
     assert(traverse(test_tree, 'root/new/branch/1') == 'd')
     traverse(test_tree, 'root/new/branch/1', value='D')
@@ -68,6 +84,11 @@ if __name__ == '__main__':
     traverse(test_tree, 'root/new/branch/-1', value=['F', 'G']) # extends value
     assert(traverse(test_tree, 'root/new/branch/2:5') == ['E', 'F', 'G']) # accepts rangers
     assert(len(traverse(test_tree, 'root/new/branch')) == 5)
+    assert(traverse(test_tree, 'root/new/branch/2/#pop') == 'E') # We can pop values from arrays
+    assert(len(traverse(test_tree, 'root/new/branch')) == 4)
+    assert(traverse(test_tree, 'root/new/branch/-1/#pop') == 'G') # relative works too
+    assert(len(traverse(test_tree, 'root/new/branch')) == 3)
+    assert(traverse(test_tree, 'root/new/branch') == ['C', 'D', 'F'])
     traverse(test_tree, 'root/new', value={'nested': {'inside' : {'another' : 'nested'}}})
     assert(traverse(test_tree, 'root/new/nested')['inside']['another'] == 'nested')
     
