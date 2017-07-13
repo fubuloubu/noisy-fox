3e81b42c07386fcdc6bd942b8ee810557f481f4f
Bryant <fubuloubu@gmail.com>
2017-07-13 12:53:13 -0700

    Updates for steps

--- timeseries/compression.py
+++ timeseries/compression.py
@@ -21,6 +21,7 @@ def compress(timeseries):
         check_update_rate(next_time, prev_time, update_rate)
         
         if next_data != prev_data:
+            compressed_timeseries.append((next_time, prev_data)) # step
             compressed_timeseries.append(next_pt)
         
         prev_pt = next_pt
@@ -41,46 +42,61 @@ def uncompress(timeseries, update_rate):
     check_tuple(prev_pt)
     uncompressed_timeseries = [prev_pt]
     
-    prev_time, prev_data = prev_pt
-
     for next_pt in timeseries[1:]:
         check_tuple(next_pt)
+        
+        prev_time, prev_data = prev_pt
         next_time, next_data = next_pt
         
+        if prev_time == next_time:
+            uncompressed_timeseries.pop() # Handle steps
+
         # For all timeseries elements between the current entry time
         # and the next, fille in with datapoints matching the current
         # data with successive time increments using update_rate
         prev_time = add_and_round(prev_time, update_rate)
         while next_time > prev_time:
-            uncompressed_timeseries.append((prev_time, prev_data))
+            prev_pt = (prev_time, prev_data)
+            uncompressed_timeseries.append(prev_pt)
             prev_time = add_and_round(prev_time, update_rate)
-
         # After we've filled up the array with previous data samples,
         # add the newest sample in there and do it again
         uncompressed_timeseries.append(next_pt)
-        prev_data = next_data
-        prev_time = next_time
+        prev_pt = next_pt
 
     return uncompressed_timeseries
 
 if __name__ == '__main__':
-    # Trviai cases
+    def assert_equal(q1, q2):
+        try:
+            assert(q1 == q2)
+        except AssertionError as error:
+            raise AssertionError('Result:\n{}\n  is not equal to:\n{}'.format(q1, q2))
+
+    # Trvial cases
     timeseries = []
-    assert(uncompress(*compress(timeseries)) == timeseries)
+    assert_equal(uncompress(*compress(timeseries)), timeseries)
     timeseries = [(0, 0)]
-    assert(uncompress(*compress(timeseries)) == timeseries)
+    assert_equal(uncompress(*compress(timeseries)), timeseries)
     timeseries = [(0, 0), (1, 0)]
-    assert(compress(timeseries) == ([(0, 0), (1, 0)], 1))
-    assert(uncompress(*compress(timeseries)) == timeseries)
+    assert_equal(compress(timeseries), ([(0, 0), (1, 0)], 1))
+    assert_equal(uncompress(*compress(timeseries)), timeseries)
+    
+    # Flat lines
     timeseries = [(0, 1), (1, 1), (2, 1), (3, 1)]
-    assert(compress(timeseries) == ([(0, 1), (3, 1)], 1))
-    assert(uncompress(*compress(timeseries)) == timeseries)
+    assert_equal(compress(timeseries), ([(0, 1), (3, 1)], 1))
+    assert_equal(uncompress(*compress(timeseries)), timeseries)
+    
+    # Steps work appropiately
+    timeseries = [(0, 0), (1, 0), (2, 2), (3, 2)]
+    assert_equal(compress(timeseries), ([(0, 0), (2, 0), (2, 2), (3, 2)], 1))
+    assert_equal(uncompress(*compress(timeseries)), timeseries)
 
     # More advanced cases
     from random import randrange as rand_range
     N = 100
     for _ in range(10): # run this many trials
         timeseries = [(t/N, rand_range(0,2)) for t in range(N)]
-        assert(uncompress(*compress(timeseries)) == timeseries)
+        assert_equal(uncompress(*compress(timeseries)), timeseries)
 
     print('compress()/uncompress() Testing Passed!')
